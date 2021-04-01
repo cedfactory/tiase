e18989d528b04203f275e73e598a73361f35f11a
cl <claugero@gmail.com>
2021-04-01 14:17:38 +0200

    add mape & rmse calculations + tests

--- lib/ml/toolbox.py
+++ lib/ml/toolbox.py
@@ -1,7 +1,32 @@
 import pandas as pd
+import numpy as np
 
 def AddTrend(df, columnSource, columnTarget):
     diff = df[columnSource] - df[columnSource].shift(1)
     df[columnTarget] = diff.gt(0).map({False: 0, True: 1})
     return df
     
+def get_mape(y_true, y_pred):
+    """
+    Compute Mean Absolute Percentage Error (MAPE)
+    INPUT:
+    y_true - actual variable
+    y_pred - predicted variable
+    OUTPUT:
+    mape - Mean Absolute Percentage Error (%)
+    """
+    y_true, y_pred = np.array(y_true), np.array(y_pred)
+    mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    return mape
+
+def get_rmse(y_true, y_pred):
+    """
+    Compute Root Mean Squared Error (RMSE)
+    INPUT:
+    y_true - actual variable
+    y_pred - predicted variable
+    OUTPUT:
+    rmse - Root Mean Squared Error
+    """
+    rmse = np.sqrt(np.mean(np.power((y_true - y_pred),2)))
+    return rmse
--- lib/test_ml_toolbox.py
+++ lib/test_ml_toolbox.py
@@ -3,8 +3,8 @@ import numpy as np
 from lib.fimport import *
 from lib.ml import toolbox
 
-class TestGetTendency:
-    def test_trend_close(self):
+class TestMlToolbox:
+    def test_get_trend_close(self):
         data = {'Values':[20, 21, 23, 19, 18, 24]}
         df = pd.DataFrame(data)
         dfWithTrends = toolbox.AddTrend(df, "Values", "Trend")
@@ -12,5 +12,19 @@ class TestGetTendency:
         equal = np.array_equal(trends, [0, 1, 1, 0, 0, 1])
         assert(equal)
 
-        
+    def test_mape(self):
+        y_true = np.array([1, 2, -4])
+        y_pred = np.array([-39, -58, 76])
+        mape = toolbox.get_mape(y_true, y_pred)
+        assert(mape == 3000)
+
+    def test_rmse(self):
+        y_true = np.array([28, 20, 30])
+        y_pred = np.array([26, 30, 16])
+        rmse = toolbox.get_rmse(y_true, y_pred)
+        assert(rmse == 10)
+ 
+  
+  
+         
 
