4ce90366d91e6cfd8fc7dfa40bb971dc6299daca
cl <claugero@gmail.com>
2021-05-07 20:29:21 +0200

    test for ml/analysis.py

---
+++ lib/test_ml_analysis.py
@@ -0,0 +1,17 @@
+import numpy as np
+from lib.ml import analysis
+
+class TestMlAnalysis:
+
+    def test_mape(self):
+        y_true = np.array([1, 2, -4])
+        y_pred = np.array([-39, -58, 76])
+        mape = analysis.get_mape(y_true, y_pred)
+        assert(mape == 3000)
+
+    def test_rmse(self):
+        y_true = np.array([28, 20, 30])
+        y_pred = np.array([26, 30, 16])
+        rmse = analysis.get_rmse(y_true, y_pred)
+        assert(rmse == 10)
+ 
