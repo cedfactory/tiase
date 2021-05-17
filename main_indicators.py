3775c9c4f5c7b4ff48ae896fb2e8987f0b9e45e8
cl <claugero@gmail.com>
2021-05-17 22:13:31 +0200

    trend stats for cac40

--- main_indicators.py
+++ main_indicators.py
@@ -1,5 +1,6 @@
 from lib.fimport import *
 from lib.findicators import *
+import os
 
 df = fimport.GetDataFrameFromCsv("./lib/data/CAC40/AI.PA.csv")
 visu.ExportFromDataframe(df,"Close","close.png")
@@ -11,3 +12,16 @@ print(df.head(20))
 
 df = findicators.remove_features(df, ['open', 'high', 'low', 'adj close', 'volume'])
 print(df.head(20))
+
+#
+# parse directory cac40
+#
+directory = "./lib/data/CAC40/"
+for filename in os.listdir(directory):
+    if filename.endswith(".csv"):
+        df = fimport.GetDataFrameFromCsv(directory+"/"+filename)
+        df = findicators.add_technical_indicators(df, ["trend"])
+        trend_counted = df['trend'].value_counts()
+        trend_ratio = 100 * trend_counted[1] / (trend_counted[1]+trend_counted[0])
+        value = filename[:len(filename)-4]
+        print("{} ({}),{:.2f}".format(value, fimport.cac40[value], trend_ratio))
