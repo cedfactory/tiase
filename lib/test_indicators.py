c374b0e0bc40416229c1dffc87d74cb380c65977
cl <claugero@gmail.com>
2021-05-17 21:58:08 +0200

    fix tests

--- lib/test_indicators.py
+++ lib/test_indicators.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class TestIndicators:
     def test_number_colums(self):
-        df = fimport.GetDataFrameFromCsv("./lib/data/CAC40/_AI.PA.csv")
+        df = fimport.GetDataFrameFromCsv("./lib/data/CAC40/AI.PA.csv")
         print(list(df.columns))
         assert(len(list(df.columns)) == 6)
         df = findicators.add_technical_indicators(df, ["macd", "rsi_30", "cci_30", "dx_30"])
--- lib/test_ml_toolbox.py
+++ lib/test_ml_toolbox.py
@@ -7,9 +7,9 @@ class TestMlToolbox:
 
     def test_get_train_test_data(self):
         data = [[10,1000], [2,500], [3,600], [2,800], [2,500], [0,700], [1,800], [3,500], [5,900], [7,1000]]
-        df = pd.DataFrame(data, columns = ['indicator', 'Adj Close'])
+        df = pd.DataFrame(data, columns = ['indicator', 'adj close'])
         df.index.name = 'Date'
-        X_train, y_train, X_test, y_test, y_normaliser = toolbox.get_train_test_data(df, 2, 0.6)
+        X_train, y_train, X_test, y_test, y_normaliser = toolbox.get_train_test_data_from_dataframe0(df, 2, 0.6)
 
         X_train_expected = np.array([[[1., 1. ], [0.2 ,0. ]], [[0.2, 0. ], [0.3, 0.2]], [[0.3, 0.2], [0.2, 0.6]], [[0.2, 0.6], [0.2, 0.]]])
         np.testing.assert_allclose(X_train, X_train_expected, 0.00001)
