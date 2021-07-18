deaf5ccf9092031ce2d5765b543099fd8d702866
cl <claugero@gmail.com>
2021-07-18 23:45:34 +0200

    fix : missing file

--- lib/ml/__init__.py
+++ lib/ml/__init__.py
@@ -2,6 +2,6 @@
 A library for ml
 """
 
-__all__ = ["toolbox","dr","lstm_hao","classifier_lstm","ml_bilstm","analysis"]
+__all__ = ["toolbox","dr","classifier_svc","lstm_hao","classifier_lstm","ml_bilstm","analysis"]
 __version__ = '0.1'
 __author__ = 'ced'
