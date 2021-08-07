6ea2184821777c64a5bbf2a98c43c2943389620b
cedfactory <claugero@gmail.com>
2021-08-07 10:21:28 +0200

    :memo: description of the general process

--- lib/alfred.py
+++ lib/alfred.py
@@ -3,6 +3,37 @@ from lib.fimport import *
 from lib.findicators import *
 from lib.ml import *
 
+'''
+process :
+*** import data ***
+- dataframe ohlcv
+- simple_rtn & target computation
+- [drop ohlvc?]
+*** data processing ***
+- outliers (\in {stdcutoff, winsorize, mam, ema}
+- indicators computation (especially if close are updated by the outliers computation)
+- transform \in {log, x2}
+- discretization \in {supervised, unsupervised}
+*** feature engineering ***
+- reduction \in {correlation, pca, rfecv, vsa}
+- balancing \in {smote}
+*** machine learning ***
+- classifier | regressor (\in {svc, xgboost, keras, ...})
+
+
+
+input :
+- value (AI.PA, ...)
+- outliers method
+- technical indicators list
+- drop ohlcv ?
+- transform {"method1" : [indicators], "method2" : [indicators]}
+- discretize {"indicator" : method + params (eg strategy for KBinsDiscretizer) + #bins (2 by default)}
+- reduction : method + threshold (0.95 by default)
+- balancing
+- learning model
+'''
+
 def execute(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
