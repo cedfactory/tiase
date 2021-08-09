17bb10d7d2dcc948babb6b5346484e293b8b11cc
cedfactory <claugero@gmail.com>
2021-08-09 08:23:19 +0200

    :broom: on duplicated code

--- main_feature_vsa.py
+++ main_feature_vsa.py
@@ -6,20 +6,14 @@ from lib.fdatapreprocessing import fdataprep
 import pandas as pd
 import os
 
-#
-# parse directory cac40
-#
 def cac40():
     directory = "./lib/data/CAC40/"
     for filename in os.listdir(directory):
         if filename.endswith(".csv"):
             df = fimport.GetDataFrameFromCsv(directory+"/"+filename)
             technical_indicators = ["trend_1d","macd","rsi_30","cci_30","williams_%r","stoch_%k","stoch_%d","er","stc"]
-            technical_indicators.extend(["sma_5","sma_10","sma_20"])
-            technical_indicators.extend(["ema_5","ema_10","ema_20"])
-            technical_indicators.extend(["wma_5","wma_10","wma_20"])
-            technical_indicators.extend(["atr","adx","roc"])
-            technical_indicators.extend(["simple_rtn", "mom", "target"])
+            technical_indicators.extend(["sma_5","sma_10","sma_20","ema_5","ema_10","ema_20","wma_5","wma_10","wma_20"])
+            technical_indicators.extend(["atr","adx","roc","simple_rtn", "mom", "target"])
 
             # technical_indicators = ["vsa"]
 
@@ -29,33 +23,13 @@ def cac40():
             # print("{} ({}),{:.2f},{:.2f},{:.2f},{:.2f},{:.2f}".format(value, fimport.cac40[value], trend_ratio, true_positive, true_negative, false_positive, false_negative))
 
             df = fdataprep.process_technical_indicators(df, ['missing_values'])
-            df = fdataprep.process_technical_indicators(df, ['outliers_cut_stdcutoff'])
-            df = fdataprep.process_technical_indicators(df, ['drop_ohlcv'])
-            df = fprocessfeature.process_features(df, ['kbest_reduction'])
+            #df = fdataprep.process_technical_indicators(df, ['outliers_cut_stdcutoff'])
+            #df = fdataprep.process_technical_indicators(df, ['drop_ohlcv'])
+            #df = fprocessfeature.process_features(df, ['kbest_reduction'])
+            print(df.head())
+            df = vsa.create_bunch_of_vsa_features(df, [1, 2, 3])
             df = fprocessfeature.process_features(df, ['vsa_reduction'])
 
-            visu.DisplayHistogramFromDataframe(df, 'simple_rtn', './tmp/' + value + '_close.png')
-
-            df_copy = df.copy()
-            df = fdataprep.process_technical_indicators(df, ['outliers_winsorize'])
-            visu.DisplayOutliersFromDataframe(df_copy, df, './tmp/' + value + '_outliers.png')
-
-            df = fdataprep.process_technical_indicators(df, ['discretization_unsupervised'])
-            df = fprocessfeature.process_features(df, ['correlation_reduction'])
-            df = fprocessfeature.process_features(df, ['smote'])
-            df = fprocessfeature.process_features(df, ['rfecv_reduction'])
-            df = fdataprep.process_technical_indicators(df, ['discretization'])
-            df = fprocessfeature.process_features(df, ['correlation_reduction'])
-            df = fprocessfeature.process_features(df, ['pca_reduction'])
-            df = fdataprep.process_technical_indicators(df, ['discretization'])
-            df = fprocessfeature.process_features(df, ['correlation_reduction'])
-            df = fdataprep.process_technical_indicators(df, ['discretization_unsupervised'])
-            df = fdataprep.process_technical_indicators(df, ['transformation_log'])
-            df = fdataprep.process_technical_indicators(df, ['transformation_x2'])
-            df = fdataprep.process_technical_indicators(df, ['outliers_stdcutoff'])
-            df = fdataprep.process_technical_indicators(df, ['outliers_ema'])
-            df = fdataprep.process_technical_indicators(df, ['outliers_mam'])
-            df = fdataprep.process_technical_indicators(df, ['outliers_winsorize'])
 
 _usage_str = """
 Options:
