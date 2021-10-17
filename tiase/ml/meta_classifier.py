d0e0be7915cd6f69f76e8b83087fa5205618dc21
exopyl <exopyl74@mail.ru>
2021-10-17 21:46:41 +0200

    :rocket!

--- tiase/ml/meta_classifier.py
+++ tiase/ml/meta_classifier.py
@@ -1,20 +1,44 @@
 from sklearn.ensemble import VotingClassifier
+from . import analysis
 
 def PrepareModelsForMetaClassifierVoting(models):
-    formatted_models = []
-    return formatted_models
+    formatted_estimators = []
+    for model in models:
+        name = model["name"]
+        estimator = model["classifier"].model
+        formatted_estimators.append((name, estimator))
+    return formatted_estimators
 
 
 class MetaClassifierVoting:
-    def __init__(self, models, params):
+    def __init__(self, classifiers, X_train, y_train, X_test, y_test, params=None):
         '''
-        format for models : [('model1_name', model1), ('model2_name', model2)]
+        format for classifiers : [('classifier1_name', classifier1), ('classifier2_name', classifier2)]
         '''
-        self.models = models
+        self.classifiers = classifiers
 
-        self.seq_len = 21
+        self.voting = "soft"
         if params:
-            self.seq_len = params.get("seq_len", self.seq_len)
+            self.voting = params.get("voting", self.voting)
+
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
 
     def build(self):
-        self.meta_model = VotingClassifier(estimators=models, voting='hard')
+        print(self.classifiers)
+        self.model = VotingClassifier(estimators=self.classifiers, voting=self.voting)
+
+    def fit(self):
+        self.model.fit(self.X_train, self.y_train)
+
+    def predict(self, x):
+        self.model.predict(x)
+
+    def get_analysis(self):
+        self.y_test_pred = self.model.predict(self.X_test)
+        self.y_test_prob = self.model.predict_proba(self.X_test)
+        self.y_test_prob = self.y_test_prob[:, 1]
+        self.analysis = analysis.classification_analysis(self.X_test, self.y_test, self.y_test_pred, self.y_test_prob)
+        return self.analysis
