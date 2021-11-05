cbcee30a16aaa26b6a467795accdcaea636ca3b8
exopyl <exopyl74@mail.ru>
2021-11-05 19:13:15 +0100

    use parameter

--- main_alfred.py
+++ main_alfred.py
@@ -1,3 +1,18 @@
 from tiase import alfred
 
-alfred.execute("alfred_config.xml")
+_usage_str = """
+Options:
+    [xmlfile]
+"""
+
+def _usage():
+    print(_usage_str)
+
+if __name__ == "__main__":
+    import sys
+    xmlfile = "alfred_config.xml"
+    if len(sys.argv) > 1:
+        xmlfile = sys.argv[1]
+    else:
+        _usage()
+    alfred.execute(xmlfile)
