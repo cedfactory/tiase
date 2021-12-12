cd1cd56ba9b1b424d57b7059a77dea6007168077
exopyl <exopyl74@mail.ru>
2021-12-12 19:16:19 +0100

    fix when bad arguments

--- main_alfred.py
+++ main_alfred.py
@@ -19,6 +19,8 @@ if __name__ == "__main__":
         elif len(sys.argv) == 3 and (sys.argv[1] == "--details" or sys.argv[1] == "-d"):
             value = sys.argv[2]
             alfred.details_for_value(value)
+        else:
+            _usage()
     else:
         _usage()
     
