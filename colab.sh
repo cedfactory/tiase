b0fc4c8c06c59b260bc331e766a798fb13743539
exopyl <exopyl74@mail.ru>
2021-12-12 17:56:09 +0100

    update

--- colab.sh
+++ colab.sh
@@ -1,1 +1,6 @@
-pip install yfinance
+# in colab, first clone this project :
+# > !git clone https://github.com/cedfactory/tiase.git
+# > %cd tiase
+# then execute this script :
+# > !sh colab.sh
+pip install -r ./requirements.txt
