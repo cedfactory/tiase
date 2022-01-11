0ee796f35f3e4b77437cde030bf161c7b75ed3b9
exopyl <exopyl74@mail.ru>
2022-01-11 18:26:08 +0100

    hyper parameters tuning with aflred

--- tiase/ml/hyper_parameters_tuning.py
+++ tiase/ml/hyper_parameters_tuning.py
@@ -1,6 +1,7 @@
 import xml.etree.cElementTree as ET
 from . import classifier,analysis
 from sklearn.model_selection import GridSearchCV
+import json
 
 class HPTGridSearch(classifier.Classifier):
     """
@@ -28,6 +29,7 @@ class HPTGridSearch(classifier.Classifier):
         self.model = GridSearchCV(estimator=self.classifier.get_model(), param_grid=self.param_grid, scoring=self.scoring)
 
     def fit(self, data_splitter):
+        self.result = None
         self.data_splitter = data_splitter
         self.build()
         self.model.fit(self.data_splitter.X_train, self.data_splitter.y_train)
@@ -35,10 +37,12 @@ class HPTGridSearch(classifier.Classifier):
         return self.model.best_params_
 
     def get_analysis(self):
+        if self.result:
+            return self.result
         y_test_pred, y_test_prob = classifier.get_pred_and_prob_with_predict_pred_and_predict_proba(self.model, self.data_splitter)
-        result = analysis.classification_analysis(self.data_splitter.X_test, self.data_splitter.y_test, y_test_pred, y_test_prob)
-        result["best_param"] = self.model.best_params_
-        return result
+        self.result = analysis.classification_analysis(self.data_splitter.X_test, self.data_splitter.y_test, y_test_pred, y_test_prob)
+        self.result["best_param"] = self.model.best_params_
+        return self.result
 
     def save(self, filename):
 
@@ -56,7 +60,16 @@ class HPTGridSearch(classifier.Classifier):
         xmlfilename = filename+'.xml'
         tree.write(xmlfilename)
 
-        print("HPTGridSearch.save() writing {}".format(xmlfilename))
-
     def load(self, filename):
-        print("HPTGridSearch.load() is not implemented")
+
+        tree = ET.parse(filename+'.xml')
+        root = tree.getroot()
+
+        self.result = {}
+        best_param = root.find('best_param').text
+        best_param = best_param.replace("'", "\"")
+        self.result["best_param"] = json.loads(best_param)
+        self.result["accuracy"] = float(root.find('accuracy').text)
+        self.result["precision"] = float(root.find('precision').text)
+        self.result["recall"] = float(root.find('recall').text)
+        self.result["f1_score"] = float(root.find('f1_score').text)
--- tiase/test_ml_hyper_parameters_tuning.py
+++ tiase/test_ml_hyper_parameters_tuning.py
@@ -2,7 +2,9 @@ import pandas as pd
 import numpy as np
 from tiase.fimport import fimport
 from tiase.findicators import findicators
-from tiase.ml import toolbox,data_splitter,classifiers_factory
+from tiase.fdatapreprocessing import fdataprep
+from tiase.ml import data_splitter,classifiers_factory
+from tiase import alfred
 import pytest
 
 class TestMlHyperParametersTuning:
@@ -16,6 +18,7 @@ class TestMlHyperParametersTuning:
         df = findicators.normalize_column_headings(df)
         df = findicators.add_technical_indicators(df, ["target"])
         df = findicators.remove_features(df, ["open","adj_close","low","high","volume"])
+        df = fdataprep.process_technical_indicators(df, ['missing_values']) # shit happens
 
         # data splitter
         ds = data_splitter.DataSplitterTrainTestSimple(df, target="target", seq_len=21)
@@ -46,23 +49,38 @@ class TestMlHyperParametersTuning:
         best_params, model_analysis = self._hpt_grid_search("decision tree")
 
         assert(best_params["criterion"] == 'gini')
-        assert(best_params["max_depth"] == 3)
+        assert(best_params["max_depth"] == 4)
         assert(best_params["splitter"] == 'best')
 
-        assert(model_analysis["accuracy"] == pytest.approx(0.5353, 0.01))
-        assert(model_analysis["precision"] == pytest.approx(0.3635, 0.01))
-        assert(model_analysis["recall"] == pytest.approx(0.3348, 0.01))
-        assert(model_analysis["f1_score"] == pytest.approx(0.2410, 0.01))
+        assert(model_analysis["accuracy"] == pytest.approx(0.5394, 0.01))
+        assert(model_analysis["precision"] == pytest.approx(0.5377, 0.01))
+        assert(model_analysis["recall"] == pytest.approx(0.9949, 0.01))
+        assert(model_analysis["f1_score"] == pytest.approx(0.6981, 0.01))
+
+    def test_HPTGridSearch_decision_tree_with_alfred(self):
+        alfred.execute("./tiase/data/test/alfred_classifier_hyper_parameters_tuning.xml")
+ 
+        hpt_grid_search = classifiers_factory.ClassifiersFactory.get_classifier("grid search")
+        hpt_grid_search.load("./tmp/hpt_1")
+        model_analysis = hpt_grid_search.get_analysis()
+        assert(model_analysis["best_param"]["criterion"] == "gini")
+        assert(model_analysis["best_param"]["max_depth"] == 4)
+        assert(model_analysis["best_param"]["random_state"] == 1)
+        assert(model_analysis["best_param"]["splitter"] == "best")
+        assert(model_analysis["accuracy"] == pytest.approx(0.54, 0.01))
+        assert(model_analysis["precision"] == pytest.approx(0.54, 0.01))
+        assert(model_analysis["recall"] == pytest.approx(0.99, 0.01))
+        assert(model_analysis["f1_score"] == pytest.approx(0.70, 0.01))
 
     def test_HPTGridSearch_lstm1(self):
         best_params, model_analysis = self._hpt_grid_search("lstm1", {'epochs': [5, 10], 'batch_size': [10, 15]})
 
         assert(best_params["epochs"] == 10)
-        assert(best_params["batch_size"] == 15)
+        assert(best_params["batch_size"] == 10)
 
-        assert(model_analysis["accuracy"] == pytest.approx(0.5340, 0.01))
-        assert(model_analysis["precision"] == pytest.approx(0.1780, 0.01))
-        assert(model_analysis["recall"] == pytest.approx(0.3333, 0.01))
-        assert(model_analysis["f1_score"] == pytest.approx(0.2321, 0.01))
+        assert(model_analysis["accuracy"] == pytest.approx(0.5353, 0.01))
+        assert(model_analysis["precision"] == pytest.approx(0.5353, 0.01))
+        assert(model_analysis["recall"] == pytest.approx(1.0000, 0.01))
+        assert(model_analysis["f1_score"] == pytest.approx(0.6973, 0.01))
 
 
