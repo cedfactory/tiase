47cc78950c5c6965f031e5319876795241a287ee
cedfactory <78565328+cedfactory@users.noreply.github.com>
2022-01-11 14:40:55 +0100

    reactivate test

--- tiase/test_ml_hyper_parameters_tuning.py
+++ tiase/test_ml_hyper_parameters_tuning.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from tiase.fimport import fimport
 from tiase.findicators import findicators
-from tiase.ml import toolbox,data_splitter,classifiers_factory,hyper_parameters_tuning
+from tiase.ml import toolbox,data_splitter,classifiers_factory
 import pytest
 
 class TestMlHyperParametersTuning:
@@ -14,7 +14,7 @@ class TestMlHyperParametersTuning:
 
         # technical indicators & target
         df = findicators.normalize_column_headings(df)
-        df = toolbox.make_target(df, "pct_change", 7)
+        df = findicators.add_technical_indicators(df, ["target"])
         df = findicators.remove_features(df, ["open","adj_close","low","high","volume"])
 
         # data splitter
@@ -28,7 +28,7 @@ class TestMlHyperParametersTuning:
             classifier.param_grid = param_grid
 
         # hyper parameters tuning
-        hpt_grid_search = hyper_parameters_tuning.HPTGridSearch({"classifier":classifier})
+        hpt_grid_search = classifiers_factory.ClassifiersFactory.get_classifier("grid search", {"classifier":classifier})
         best_params = hpt_grid_search.fit(ds)
         print(best_params)
 
@@ -45,26 +45,24 @@ class TestMlHyperParametersTuning:
     def test_HPTGridSearch_decision_tree(self):
         best_params, model_analysis = self._hpt_grid_search("decision tree")
 
-        assert(best_params["criterion"] == 'entropy')
-        assert(best_params["max_depth"] == 2)
+        assert(best_params["criterion"] == 'gini')
+        assert(best_params["max_depth"] == 3)
         assert(best_params["splitter"] == 'best')
 
-        assert(model_analysis["accuracy"] == pytest.approx(0.389, 0.01))
-        assert(model_analysis["precision"] == pytest.approx(1., 0.01))
-        assert(model_analysis["recall"] == pytest.approx(.0175, 0.01))
-        assert(model_analysis["f1_score"] == pytest.approx(.0345, 0.01))
+        assert(model_analysis["accuracy"] == pytest.approx(0.5353, 0.01))
+        assert(model_analysis["precision"] == pytest.approx(0.3635, 0.01))
+        assert(model_analysis["recall"] == pytest.approx(0.3348, 0.01))
+        assert(model_analysis["f1_score"] == pytest.approx(0.2410, 0.01))
 
     def test_HPTGridSearch_lstm1(self):
         best_params, model_analysis = self._hpt_grid_search("lstm1", {'epochs': [5, 10], 'batch_size': [10, 15]})
 
-        # todo  : investigate why the results are not reproductible
-        '''
-        assert(best_params["epochs"] == 15)
-        assert(best_params["batch_size"] == 10)
+        assert(best_params["epochs"] == 10)
+        assert(best_params["batch_size"] == 15)
 
-        assert(model_analysis["accuracy"] == pytest.approx(0.6213, 0.01))
-        assert(model_analysis["precision"] == pytest.approx(0.6213, 0.01))
-        assert(model_analysis["recall"] == pytest.approx(1.0000, 0.01))
-        assert(model_analysis["f1_score"] == pytest.approx(0.7664, 0.01))
-        '''
+        assert(model_analysis["accuracy"] == pytest.approx(0.5340, 0.01))
+        assert(model_analysis["precision"] == pytest.approx(0.1780, 0.01))
+        assert(model_analysis["recall"] == pytest.approx(0.3333, 0.01))
+        assert(model_analysis["f1_score"] == pytest.approx(0.2321, 0.01))
+
 
